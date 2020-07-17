2bb51dd05a07846c226829ab29afb95acc5d97d0
nbankes <nbhankes@gmail.com>
2020-07-16 21:12:22 -0700

    Initial clone

---
+++ hackernews-node/src/index.js
@@ -0,0 +1,48 @@
+//https://www.howtographql.com/graphql-js/3-a-simple-mutation/
+
+const { GraphQLServer } = require("graphql-yoga");
+const { PrismaClient } = require("@prisma/client");
+const { PubSub } = require("graphql-yoga");
+
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
+
+const prisma = new PrismaClient();
+const pubsub = new PubSub();
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+// 1
+let idCount = links.length;
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote,
+};
+
+const server = new GraphQLServer({
+  typeDefs: __dirname + "/schema.graphql",
+  resolvers,
+  context: (request) => {
+    return {
+      ...request,
+      prisma,
+      pubsub,
+    };
+  },
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
